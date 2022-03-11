11561fc8637afe9c14e8823fbcd2b0b21b734117
veraborvinski <72395680+veraborvinski@users.noreply.github.com>
2022-03-11 20:29:49 +0000

    Add files via upload
    Forms version of the sample code

---
+++ ATM/ATM/ATMForm.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATM
+{
+    public partial class ATMForm : Form
+    {
+        //local referance to the array of accounts
+        private Account[] ac = new Account[3];
+
+        String input;
+        int acNum = 0;
+        int pin = 0;
+
+        Label textControlMain = new Label();
+        Label textControl1 = new Label();
+        Label textControl2 = new Label();
+        Label textControl3 = new Label();
+
+        //this is a referance to the account that is being used
+        private Account activeAccount = null;
+
+        Button[,] btn = new Button[4, 4];
+        Button[,] ctrl = new Button[2, 4];
+        public ATMForm()
+        {
+            InitializeComponent();
+
+            input = "";
+
+            //how to add text to panels: https://stackoverflow.com/questions/57994165/how-can-i-write-text-on-a-panel-in-c
+
+            textControlMain.Location = new Point(panel1.Location.X - 225, panel1.Location.Y + 20);
+            textControlMain.ForeColor = Color.White;
+            panel1.Controls.Add(textControlMain);
+            textControl1.Location = new Point(panel1.Location.X - 225, textControlMain.Location.Y + 50);
+            textControl1.ForeColor = Color.White;
+            panel1.Controls.Add(textControl1);
+            textControl2.Location = new Point(panel1.Location.X - 225, textControl1.Location.Y + 50);
+            textControl2.ForeColor = Color.White;
+            panel1.Controls.Add(textControl2);
+
+            int count = 1;
+            for (int y = 0; y < 4; y++)
+            {
+                for (int x = 0; x < 4; x++)
+                {
+                    btn[x, y] = new Button();
+                    btn[x, y].SetBounds(250 + (75 * x), 225 + (55 * y), 65, 45);
+                    btn[x, y].BackColor = Color.Gray;
+                    btn[x, y].Text = Convert.ToString(count);
+                    btn[x, y].Click += new EventHandler(this.btnEvent_Click); 
+                    Controls.Add(btn[x, y]);
+                    count += (x < 3 && y < 3) ? 1 : 0;
+                }
+            }
+            btn[0, 3].Text = "";
+            btn[1, 3].Text = "0";
+            btn[2, 3].Text = "";
+            btn[3, 3].Text = "";
+            btn[3, 3].Text = "";
+            btn[3, 0].Text = "cancel";
+            btn[3, 1].Text = "clear";
+            btn[3, 2].Text = "enter";
+            btn[3, 0].BackColor = Color.Red;
+            btn[3, 1].BackColor = Color.Orange;
+            btn[3, 2].BackColor = Color.Green;
+            for (int y = 0; y < 4; y++)
+            {
+                for (int x = 0; x < 2; x++)
+                {
+                    ctrl[x, y] = new Button();
+                    ctrl[x, y].SetBounds(160 + (400 * x), 5 + (55 * y), 65, 45);
+                    ctrl[x, y].BackColor = Color.Gray;
+                    ctrl[x, y].Text = (x == 0) ? ">" : "<";
+                    ctrl[x, y].Click += new EventHandler(this.ctrlEvent_Click);
+                    Controls.Add(ctrl[x, y]);
+                    count++;
+                }
+            }
+            ac[0] = new Account(300, 1111, 111111);
+            ac[1] = new Account(750, 2222, 222222);
+            ac[2] = new Account(3000, 3333, 333333);
+
+            if(activeAccount == null)
+            {
+                textControlMain.Text = "enter your account number..";
+            }
+        }
+
+        /*
+         *    this method checks if input matches an existing account
+         * 
+         */
+        private Account findAccount()
+        {
+
+            for (int i = 0; i < ac.Length; i++)
+            {
+                if (ac[i].getAccountNum() == Convert.ToInt32(acNum))
+                {
+                    return ac[i];
+                }
+            }
+
+            return null;
+        }
+
+        /*
+         * 
+         *  give the use the options to do with the account
+         *  
+         *  
+         */
+        private void dispOptions()
+        {
+            textControlMain.Text = "take out cash";
+            textControl1.Text =  "balance";
+            textControl2.Text = "exit";
+        }
+
+        /*
+         * 
+         * offer withdrawable amounts
+         * 
+         * 
+         */
+        private void dispWithdraw()
+        {
+            textControlMain.Text ="10";
+            textControl1.Text = "50";
+            textControl2.Text = "500";
+
+        }
+        /*
+         *  display balance of activeAccount and await keypress
+         *  
+         */
+        private void dispBalance()
+        {
+            if (this.activeAccount != null)
+            {
+                textControlMain.Text = "balance " + activeAccount.getBalance();
+                textControl1.Text = "";
+                textControl2.Text = "exit";
+            }
+        }
+
+        void btnEvent_Click(object sender, EventArgs e)
+        {
+
+            int test;
+            if (int.TryParse(((Button)sender).Text, out test))
+            {
+                input += ((Button)sender).Text;
+            }
+            else if (((Button)sender) == btn[3, 2])
+            {
+                if (input.Length == 6)
+                {
+                    acNum = Convert.ToInt32(input);
+                    activeAccount = findAccount();
+                    textControlMain.Text = (activeAccount != null) ? "enter pin" : "enter your account number..";
+                    input = "";
+                }
+                else if (input.Length == 4)
+                {
+                    pin = Convert.ToInt32(input);
+                    if (activeAccount.checkPin(pin))
+                    {
+                        dispOptions();
+                    }
+                }
+            }
+            else if (((Button)sender) == btn[3, 1])
+            {
+                input = "";
+            }
+            else if (((Button)sender) == btn[3, 0])
+            {
+                textControlMain.Text = ("enter your account number..");
+                textControl1.Text = "";
+                textControl2.Text = "";
+                input = "";
+                acNum = 0;
+                pin = 0;
+                activeAccount = null;
+            }
+        }
+            
+        void ctrlEvent_Click(object sender, EventArgs e) {
+            if (((Button)sender) == ctrl[0, 0] && textControlMain.Text == "take out cash")
+            {
+                dispWithdraw();
+            }
+            else if (((Button)sender) == ctrl[0, 1] && textControl1.Text == "balance")
+            {
+                dispBalance();
+            }
+            //opiton one is entered by the user
+            else if (((Button)sender) == ctrl[0, 0] && textControlMain.Text == "10")
+            {
+
+                //attempt to decrement account by 10 punds
+                if (activeAccount.decrementBalance(10))
+                {
+                    //if this is possible display new balance and await key press
+                    textControlMain.Text = "new balance " + activeAccount.getBalance();
+                    textControl1.Text = "";
+                    textControl2.Text = "exit";
+                }
+                else
+                {
+                    //if this is not possible inform user and await key press
+                    textControlMain.Text = ("insufficent funds");
+                    textControl1.Text = "";
+                    textControl2.Text = "exit";
+                }
+            }
+            else if (((Button)sender) == ctrl[0, 1] && textControl1.Text == "50")
+            {
+                if (activeAccount.decrementBalance(50))
+                {
+                    textControlMain.Text = "new balance " + activeAccount.getBalance();
+                    textControl1.Text = "";
+                    textControl2.Text = "exit";
+                }
+                else
+                {
+                    textControlMain.Text = ("insufficent funds");
+                    textControl1.Text = "";
+                    textControl2.Text = "exit";
+                }
+            }
+            else if (((Button)sender) == ctrl[0, 2] && textControl2.Text == "500")
+            {
+                if (activeAccount.decrementBalance(500))
+                {
+                    textControlMain.Text = "new balance " + activeAccount.getBalance();
+                    textControl1.Text = "";
+                    textControl2.Text = "exit";
+                }
+                else
+                {
+                    textControlMain.Text = ("insufficent funds");
+                    textControl1.Text = "";
+                    textControl2.Text = "exit";
+                }
+
+            }
+            else if (((Button)sender) == ctrl[0, 2] && textControl2.Text == "exit")
+            {
+                textControlMain.Text = ("enter your account number..");
+                textControl1.Text = "";
+                textControl2.Text = "";
+                input = "";
+                acNum = 0;
+                pin = 0;
+                activeAccount = null;
+            }
+        }
+
+        private void ATMForm_Load(object sender, EventArgs e)     
+        {
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ ATM/ATM/Account.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class Account
+    {
+        //the attributes for the account
+        private int balance;
+        private int pin;
+        private int accountNum;
+
+        // a constructor that takes initial values for each of the attributes (balance, pin, accountNumber)
+        public Account(int balance, int pin, int accountNum)
+        {
+            this.balance = balance;
+            this.pin = pin;
+            this.accountNum = accountNum;
+        }
+
+        //getter and setter functions for balance
+        public int getBalance()
+        {
+            return balance;
+        }
+        public void setBalance(int newBalance)
+        {
+            this.balance = newBalance;
+        }
+
+        /*
+         *   This funciton allows us to decrement the balance of an account
+         *   it perfomes a simple check to ensure the balance is greater tha
+         *   the amount being debeted
+         *   
+         *   returns:
+         *   true if the transactions if possible
+         *   false if there are insufficent funds in the account
+         */
+        public Boolean decrementBalance(int amount)
+        {
+            if (this.balance > amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /*
+         * This funciton check the account pin against the argument passed to it
+         *
+         * returns:
+         * true if they match
+         * false if they do not
+         */
+        public Boolean checkPin(int pinEntered)
+        {
+            if (pinEntered == pin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int getAccountNum()
+        {
+            return accountNum;
+        }
+    }
+}
